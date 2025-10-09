3844175b2d4c63d82f84ffc8998a63d62800a6dc
IllonaSab <sabouillona@gmail.com>
2025-10-09 10:14:34 +0200

    Affichage Hello World

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center mt-20">
+    <h1>Hello World 👋</h1>
+  </div>
+</template>
+
+<script setup>
+// Pas besoin de script ici pour le moment
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #00dc82; /* vert Nuxt */
+}
+</style>
